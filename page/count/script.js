4294bd2af4514d6d5efa1b4dc7570f47ff964fc2
wanderer <56039464+wanderer-00@users.noreply.github.com>
2025-07-09 15:45:30 +0300

    -

---
+++ page/count/script.js
@@ -0,0 +1,13 @@
+var inputCount = document.getElementById('count');
+
+function plus() {
+    inputCount.value = parseInt(inputCount.value) + 1
+};
+
+function minus() {
+    if(inputCount.value == 1){
+        alert('😡');
+    } else {
+        inputCount.value -= 1
+    }
+};
